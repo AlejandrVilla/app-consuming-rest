5db5eae9f479429b956f14e4d27702491897edf9
Avherrera <avillah@unsa.edu.pe>
2024-01-28 17:51:47 -0500

    jenkinsfiles

--- consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -29,7 +29,7 @@ public class ConsumingRestApplication {
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
 		return args -> {
 			Quote quote = restTemplate.getForObject(
-					"http://localhost:8080/api/random", Quote.class);
+					"http://localhost:8090/api/random", Quote.class);
 			log.info(quote.toString());
 		};
 	}
